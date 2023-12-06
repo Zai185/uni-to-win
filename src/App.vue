7612405092a3a9b966a091d430dfa60e0ac7cd8e
Zai185 <mkhant185@gmail.com>
2023-12-06 09:38:54 +0630

    Update UI/ move to vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,107 @@
 <template>
-  <input type="text" placeholder="Uni to Win" v-model="text">
-  <p>{{ textMod }}</p>
+  <div class="flex">
+    <textarea placeholder="Uni to Win" v-model="text"></textarea>
+    <textarea ref="textBox" readonly v-model="textMod"></textarea>
+  </div>
+  <button @click="copyToClipboard">Copy</button>
 </template>
 
-<script>
-console.log('‌ေ' + 'က')
-export default {
-  data() {
-    return {
-      text: '',
-    }
-  },
-  computed: {
-    textMod() {
-      return this.text
-        .replaceAll('ßS', 'Q')
-        .replaceAll('ßG', 'R')
-        .replaceAll('ßT', 'W')
-        .replaceAll('êud', 'BudK')
-        .replaceAll('êdC', 'BCdK')
-        .replaceAll('êqd', 'BqdK')
-        .replaceAll('êod', 'BodK')
-        .replaceAll('êwd', 'BwdK')
-        .replaceAll('êxd', 'BxdK')
-        .replaceAll('êBd', 'BBdK')
-        .replaceAll('ê,d', 'B,dK')
-        .replaceAll('êvd', 'BvdK')//end
-        .replaceAll('êuH', 'BuHK')
-        .replaceAll('êCH', 'BCHK')
-        .replaceAll('êqH', 'BqHK')
-        .replaceAll('êoH', 'BoHK')
-        .replaceAll('êBH', 'BHK')
-        .replaceAll('êxH', 'BxHK')
-        .replaceAll('êBH', 'BBHK')
-        .replaceAll('ê,H', 'B,HK')
-        .replaceAll('êvH', 'BvHK')
-        .replaceAll('êu', 'BuK')
-        .replaceAll('êC', 'BCK')
-        .replaceAll('êq', 'BqK')
-        .replaceAll('êo', 'BoK')
-        .replaceAll('êw', 'BwK')
-        .replaceAll('êx', 'BxK')
-        .replaceAll('ê,', 'B,K')
-        .replaceAll('êv', 'BvK') //end
-        .replaceAll('ûcH', 'jcHK')
-        .replaceAll('û*H', 'j*HK')
-        .replaceAll('ûiH', 'jiHK')
-        .replaceAll('ûpH', 'jpHK')
-        .replaceAll('ûZH', 'jZHK')
-        .replaceAll("û'H", "j'HK")
-        .replaceAll('û"H', 'j"HK')
-        .replaceAll('ûuH', 'juHK')
-        .replaceAll('ûzH', 'jzHK')
-        .replaceAll('ûAH', 'jAHK')
-        .replaceAll('ûrH', 'jrHK')
-        .replaceAll('ûEH', 'jEHK')
-        .replaceAll('û0H', 'j0HK')//Hend
-        .replaceAll('ûyH', 'jyHK')//Hend
-        .replaceAll('ûcd', 'jcdK')
-        .replaceAll('û*d', 'j*dK')
-        .replaceAll('ûid', 'jidK')
-        .replaceAll('ûpd', 'jpdK')
-        .replaceAll('ûZd', 'jZdK')
-        .replaceAll("û'd", "j'dK")
-        .replaceAll('û"d', 'j"dK')
-        .replaceAll('ûud', 'judK')
-        .replaceAll('ûzd', 'jzdK')
-        .replaceAll('ûAd', 'jAdK')
-        .replaceAll('ûrd', 'jrdK')
-        .replaceAll('ûEd', 'jEdK')
-        .replaceAll('û0d', 'j0dK')//Hend
-        .replaceAll('ûyd', 'jydK')//Hend
-        .replaceAll('ûc', 'jcK')
-        .replaceAll('û*', 'j*K')
-        .replaceAll('ûi', 'jiK')
-        .replaceAll('ûp', 'jpK')
-        .replaceAll('ûZ', 'jZK')
-        .replaceAll("û'", "j'K")
-        .replaceAll('û"', 'j"K')
-        .replaceAll('ûu', 'juK')
-        .replaceAll('ûz', 'jzK')
-        .replaceAll('ûA', 'jAK')
-        .replaceAll('ûr', 'jrK')
-        .replaceAll('ûE', 'jEK')
-        .replaceAll('û0', 'j0K')//Hend
-        .replaceAll('ûy', 'jyK')//Hend
+<script setup>
 
-    }
-  }
+import { ref, computed } from 'vue'
+const text = ref('')
+const textBox = ref(null)
+const textMod = computed(() => {
+  return text.value
+    .replaceAll('ßS', 'Q')
+    .replaceAll('ßG', 'R')
+    .replaceAll('ßT', 'W')
+    .replaceAll('êud', 'BudK')
+    .replaceAll('êdC', 'BCdK')
+    .replaceAll('êqd', 'BqdK')
+    .replaceAll('êod', 'BodK')
+    .replaceAll('êwd', 'BwdK')
+    .replaceAll('êxd', 'BxdK')
+    .replaceAll('êBd', 'BBdK')
+    .replaceAll('ê,d', 'B,dK')
+    .replaceAll('êvd', 'BvdK')//end
+    .replaceAll('êuH', 'BuHK')
+    .replaceAll('êCH', 'BCHK')
+    .replaceAll('êqH', 'BqHK')
+    .replaceAll('êoH', 'BoHK')
+    .replaceAll('êBH', 'BHK')
+    .replaceAll('êxH', 'BxHK')
+    .replaceAll('êBH', 'BBHK')
+    .replaceAll('ê,H', 'B,HK')
+    .replaceAll('êvH', 'BvHK')
+    .replaceAll('êu', 'BuK')
+    .replaceAll('êC', 'BCK')
+    .replaceAll('êq', 'BqK')
+    .replaceAll('êo', 'BoK')
+    .replaceAll('êw', 'BwK')
+    .replaceAll('êx', 'BxK')
+    .replaceAll('ê,', 'B,K')
+    .replaceAll('êv', 'BvK') //end
+    .replaceAll('ûcH', 'jcHK')
+    .replaceAll('û*H', 'j*HK')
+    .replaceAll('ûiH', 'jiHK')
+    .replaceAll('ûpH', 'jpHK')
+    .replaceAll('ûZH', 'jZHK')
+    .replaceAll("û'H", "j'HK")
+    .replaceAll('û"H', 'j"HK')
+    .replaceAll('ûuH', 'juHK')
+    .replaceAll('ûzH', 'jzHK')
+    .replaceAll('ûAH', 'jAHK')
+    .replaceAll('ûrH', 'jrHK')
+    .replaceAll('ûEH', 'jEHK')
+    .replaceAll('û0H', 'j0HK')//Hend
+    .replaceAll('ûyH', 'jyHK')//Hend
+    .replaceAll('ûcd', 'jcdK')
+    .replaceAll('û*d', 'j*dK')
+    .replaceAll('ûid', 'jidK')
+    .replaceAll('ûpd', 'jpdK')
+    .replaceAll('ûZd', 'jZdK')
+    .replaceAll("û'd", "j'dK")
+    .replaceAll('û"d', 'j"dK')
+    .replaceAll('ûud', 'judK')
+    .replaceAll('ûzd', 'jzdK')
+    .replaceAll('ûAd', 'jAdK')
+    .replaceAll('ûrd', 'jrdK')
+    .replaceAll('ûEd', 'jEdK')
+    .replaceAll('û0d', 'j0dK')//Hend
+    .replaceAll('ûyd', 'jydK')//Hend
+    .replaceAll('ûc', 'jcK')
+    .replaceAll('û*', 'j*K')
+    .replaceAll('ûi', 'jiK')
+    .replaceAll('ûp', 'jpK')
+    .replaceAll('ûZ', 'jZK')
+    .replaceAll("û'", "j'K")
+    .replaceAll('û"', 'j"K')
+    .replaceAll('ûu', 'juK')
+    .replaceAll('ûz', 'jzK')
+    .replaceAll('ûA', 'jAK')
+    .replaceAll('ûr', 'jrK')
+    .replaceAll('ûE', 'jEK')
+    .replaceAll('û0', 'j0K')//Hend
+    .replaceAll('ûy', 'jyK')//Hend
+})
+function copyToClipboard() {
+  textBox.value.select()
+  textBox.value.setSelectionRange(0, text.value.length + 1)
+  navigator.clipboard.writeText(text.value)
 }
+
 </script>
 <style scoped>
-input {
-  font-size: 24px;
+.flex {
+  display: flex;
+  align-items: center;
 }
 
-p {
-  width: 250px;
-  height: 250px;
+textarea {
+  margin: 0 24px;
+  width: min(500px, 40vw);
+  height: 80vh;
   border: 1px solid black;
   overflow-y: scroll;
   padding: 5px;
@@ -105,71 +109,3 @@ p {
   word-wrap: break-word;
 }
 </style>
-
-ကေ
-ခေ
-ဂေ
-ဃေ
-ငေ
-စေ
-ဆေ
-ဇေ
-ဈေ
-‌ညေ
-ဋေ
-‌ဌေ
-ဍေ
-ဎေ
-ဏေ
-တေ
-ထေ
-ဒေ
-ဓေ
-နေ
-ပေ
-ဖေ
-ဗေ
-ဘေ
-မေ
-ယေ
-ရေ
-လေ
-ဝေ
-သေ
-ဟေ
-ဠေ
-အေ
-
-u
-c
-*
-C
-i
-p
-q
-Z
-s
-n
-#
-X
-!
-¡
-P
-w
-x
-'
-"
-e
-y
-Z
-A
-b
-r
-,
-&
-v
-0
-o
-[
-V
-t
